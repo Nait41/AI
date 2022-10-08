408666c76d8ceaff12cb748e0f0fa3095331f0c5
Nait <floop.41@mail.ru>
2022-10-08 16:43:45 +0300

    ...

---
+++ src/domain/TablesData.java
@@ -0,0 +1,31 @@
+package domain;
+
+public class TablesData {
+    String firstColumn;
+    String secondColumn;
+    String thirdColumn;
+
+    public String getFirstColumn() {
+        return firstColumn;
+    }
+
+    public void setFirstColumn(String firstColumn) {
+        this.firstColumn = firstColumn;
+    }
+
+    public String getSecondColumn() {
+        return secondColumn;
+    }
+
+    public void setSecondColumn(String secondColumn) {
+        this.secondColumn = secondColumn;
+    }
+
+    public String getThirdColumn() {
+        return thirdColumn;
+    }
+
+    public void setThirdColumn(String thirdColumn) {
+        this.thirdColumn = thirdColumn;
+    }
+}
